69d4f4b6a698809605756a7d6cae7dd380280ed4
Marcelo Barsotti <mbarsott@gmail.com>
2019-12-08 18:44:55 -0500

    6. A Tale of Two Databases

--- plugins.js
+++ plugins.js
@@ -1,27 +1,31 @@
-const Cookie = require('@hapi/cookie')
-const Good = require('@hapi/good')
-const Handlebars = require('handlebars')
-const Inert = require('@hapi/inert')
-const Vision = require('@hapi/vision')
+const Cookie = require("@hapi/cookie");
+const Good = require("@hapi/good");
+const Handlebars = require("handlebars");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
 
-const GoodFile = require('./lib/good-file')
-const mockData = require('./data/mock')
+const GoodFile = require("./lib/good-file");
+const mockData = require("./data/mock");
 
 const goodOptions = {
   reporters: {
-    file: [{
-      module: '@hapi/good-squeeze',
-      name: 'Squeeze',
-      args: [{ response: '*', request: '*', error: '*' }]
-    }, {
-      module: '@hapi/good-squeeze',
-      name: 'SafeJson'
-    }, {
-      module: GoodFile,
-      args: ['./log/hapi_log']
-    }]
+    file: [
+      {
+        module: "@hapi/good-squeeze",
+        name: "Squeeze",
+        args: [{ response: "*", request: "*", error: "*" }]
+      },
+      {
+        module: "@hapi/good-squeeze",
+        name: "SafeJson"
+      },
+      {
+        module: GoodFile,
+        args: ["./log/hapi_log"]
+      }
+    ]
   }
-}
+};
 
 module.exports.register = async server => {
   // register plugins
@@ -33,7 +37,7 @@ module.exports.register = async server => {
       plugin: Good,
       options: goodOptions
     }
-  ])
+  ]);
 
   // setup template rendering
   server.views({
@@ -42,43 +46,43 @@ module.exports.register = async server => {
     },
     layout: true,
     relativeTo: __dirname,
-    path: './templates',
-    partialsPath: './templates/partials',
-    helpersPath: './templates/helpers'
-  })
+    path: "./templates",
+    partialsPath: "./templates/partials",
+    helpersPath: "./templates/helpers"
+  });
 
   // setup cache
   const cache = server.cache({
-    segment: 'sessions',
+    segment: "sessions",
     expiresIn: 24 * 60 * 60 * 1000
-  })
-  server.app.cache = cache
+  });
+  server.app.cache = cache;
 
   // setup authentication/session handling
-  const redirectPath = '/login'
+  const redirectPath = "/login";
 
-  server.auth.strategy('session', 'cookie', {
+  server.auth.strategy("session", "cookie", {
     cookie: {
       isSecure: false,
-      name: 'pzz4lyfe',
-      password: 'password-should-be-32-characters'
+      name: "pzz4lyfe",
+      password: "password-should-be-32-characters"
     },
     redirectTo: redirectPath,
     appendNext: true,
     validateFunc: async (request, session) => {
-      const cached = await cache.get(session.sid)
+      const cached = await cache.get(session.sid);
       if (!cached) {
-        return { valid: false }
+        return { valid: false };
       }
       return {
         credentials: cached.account,
         valid: true
-      }
+      };
     }
-  })
+  });
 
-  server.auth.default('session')
+  server.auth.default("session");
 
-  // setup data
-  mockData.hydrate()
-}
+  // // setup data
+  // mockData.hydrate()
+};
